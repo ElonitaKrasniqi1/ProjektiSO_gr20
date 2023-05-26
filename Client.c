e606443de35b9b67222679b7bf5dab6c59de7bb1
Elvira Metaj <118827510+elvirametaj@users.noreply.github.com>
2023-05-26 20:44:37 +0200

    Update Client.c

--- Client.c
+++ Client.c
@@ -10,6 +10,22 @@
 #include <arpa/inet.h>
 #include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
 
+#define VERSION 1.0.5
+
+// Printon mesazhin e gabimit dhe mbyll programin
+void error(const char *msg){
+  perror(msg);
+  exit(EXIT_FAILURE);
+}
+int main(int argc, char **argv)
+{
+    char buffer[255];
+    char name[15], newname[15];    //Emri deri ne 15 karaktere
+    int client_socket, nameval, msgval, sd; // mbajme vlerat e soketeve dhe ato te kthyera nga leximi dhe shkrimi
+    int activity;                    // Perdoret me select() per te kontrolluar per mesazhe te reja te ardhura
+    struct addrinfo address, *res, *p;
+
+
 
 
 
